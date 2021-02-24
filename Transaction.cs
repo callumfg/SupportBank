bf2dab7ca6cd9a2ee4bf891c54b3250022670b69
Emily Patsko <Emily.Patsko@softwire.com>
2021-02-24 15:41:18 +0000

    Reads in a JSON file instead of csv

--- Transaction.cs
+++ Transaction.cs
@@ -1,33 +1,38 @@
 using System;
 using NLog;
+using Newtonsoft.Json;
 
 namespace SupportBank {   
     class Transaction {
         private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+        [JsonProperty]
         public DateTime Date { get; set; }
+        [JsonProperty("FromAccount")]
         public string From { get; set; }
+        [JsonProperty("ToAccount")]
         public string To { get; set; }
+        [JsonProperty]
         public string Narrative { get; set; }
+        [JsonProperty]
         public double Amount { get; set; }
 
         // Constructor
-        public Transaction(string[] args) {
-             try {
-                Date = Convert.ToDateTime(args[0]);
-            } 
-            catch (Exception ) {
-                Logger.Error($"{args[0]} is not a valid date.");
-                
-            } 
-            From = args[1];
-            To = args[2];
-            Narrative = args[3];
-            try {
-                Amount = Convert.ToDouble(args[4]);
-            } 
-            catch (Exception) {
-                Logger.Error($"{args[4]} is not a valid amount.");
-            }            
-        }
+        // public Transaction(string[] args) {
+        //      try {
+        //         Date = Convert.ToDateTime(args[0]);
+        //     } 
+        //     catch (Exception ) {
+        //         Logger.Error($"{args[0]} is not a valid date.");                
+        //     } 
+        //     From = args[1];
+        //     To = args[2];
+        //     Narrative = args[3];
+        //     try {
+        //         Amount = Convert.ToDouble(args[4]);
+        //     } 
+        //     catch (Exception) {
+        //         Logger.Error($"{args[4]} is not a valid amount.");
+        //     }            
+        // }
     }
 }
